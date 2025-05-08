acb1484bb8a2fff83f9142ca5ca3beb65e646089
caamomilaa <c.velazquezdamico@gmail.com>
2025-05-08 13:03:37 +0200

    finished

--- src/components/user-component/UserContainer.jsx
+++ src/components/user-component/UserContainer.jsx
@@ -1,40 +1,47 @@
 import { useState } from 'react';
 import { USERS } from '../constants/users';
-import styles from './user-container.module.css';
+// import styles from './user-container.module.css';
 
 const UserContainer = () => {
-  const [position, setPosition] = useState(0);
-  return (
-    <article>
-      <img src={USERS[position].profileImage} alt='' />
-      <div>
-        <span>Name:</span>
-        <span>{USERS[position].name}</span>
-      </div>
-      <div>
-        <span>Username:</span>
-        <span>{USERS[position].username}</span>
-      </div>
-      <div>
-        <span>Email:</span>
-        <span>{USERS[position].email}</span>
-      </div>
-      <button onClick={() => previousPosition(position, setPosition)}>
-        Previous
-      </button>
-      <button onClick={() => nextPosition(position, setPosition)}>Next</button>
-    </article>
-  );
+	const [position, setPosition] = useState(0);
+	return (
+		<article>
+			<img src={USERS[position].profileImage} alt='' />
+			<div>
+				<span>Name:</span>
+				<span>{USERS[position].name}</span>
+			</div>
+			<div>
+				<span>Username:</span>
+				<span>{USERS[position].username}</span>
+			</div>
+			<div>
+				<span>Email:</span>
+				<span>{USERS[position].email}</span>
+			</div>
+			<button
+				disabled={position <= 0}
+				onClick={() => previousPosition(position, setPosition)}
+			>
+				Previous
+			</button>
+			<button
+				disabled={position >= USERS.length - 1}
+				onClick={() => nextPosition(position, setPosition)}
+			>
+				Next
+			</button>
+		</article>
+	);
 };
 
 const nextPosition = (position, setPosition) => {
-  setPosition(position + 1);
+	setPosition(position + 1);
 };
 
 const previousPosition = (position, setPosition) => {
-  setPosition(position - 1);
-  if (USERS[position] === 0) {
-    console.log('final');
-  }
+	setPosition(position - 1);
+	if (USERS[position] === 0) {
+	}
 };
 export default UserContainer;
